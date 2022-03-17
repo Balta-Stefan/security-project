d415fc4efe371da3b70e23bce7f936fb71aebf8e
Balta-Stefan <pivad@protonmail.ch>
2022-03-17 12:00:32 +0100

    Added Async annotation to UserService application.

--- Security project/user-service/src/main/java/sni/user_service/UserApplication.java
+++ Security project/user-service/src/main/java/sni/user_service/UserApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(scanBasePackages = {"sni.common", "sni.user_service"})
 @EntityScan(basePackages = "sni.common")
 @EnableJpaRepositories(basePackages = "sni.common")
 @PropertySource("user-service-application.properties")
+@EnableAsync
 public class UserApplication
 {
     public static void main(String[] args)
