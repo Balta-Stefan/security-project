e4d3f821418699bf49f51e87cccc39793cd4fedc
Balta-Stefan <pivad@protonmail.ch>
2022-03-17 10:28:52 +0100

    Changed module structure.Removed CommonApplication SpringBootApplication.java - consequence of this is that all SpringBootApplications that require common module now require EntityScan and EnableJpaRepositories annotations.

--- Security project/admin-service/src/main/java/sni/admin_service/AdminApplication.java
+++ Security project/admin-service/src/main/java/sni/admin_service/AdminApplication.java
@@ -2,9 +2,13 @@ package sni.admin_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"sni.common", "sni.admin_service"})
+@EntityScan(basePackages = "sni.common")
+@EnableJpaRepositories(basePackages = "sni.common")
 @PropertySource("admin-service-application.properties")
 public class AdminApplication
 {
--- Security project/user-service/src/main/java/sni/user_service/UserApplication.java
+++ Security project/user-service/src/main/java/sni/user_service/UserApplication.java
@@ -2,9 +2,13 @@ package sni.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"sni.common", "sni.user_service"})
+@EntityScan(basePackages = "sni.common")
+@EnableJpaRepositories(basePackages = "sni.common")
 @PropertySource("user-service-application.properties")
 public class UserApplication
 {
